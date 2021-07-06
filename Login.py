ec085fa434d87ef8b0b165dca9361c9f7c34bbf1
AbhishekDhakad18 <abhishek.dhakad121@gmail.com>
2021-07-06 18:30:37 +0530

    improved time

--- Login.py
+++ Login.py
@@ -14,7 +14,7 @@ def loggin(driver,username,password):
 		driver.find_element_by_xpath('//button[@type="submit"]').click()
 		element=WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/section/main/div/div/div/div/button')))
 		element.click()
-		sleep(5)
+		sleep(4)
 		driver.find_element_by_xpath('//button[text()="Not Now"]').click()
 		sleep(1)
 		print("Logged in Successfully")
